3e1f4d87c807804077ae6ea7f58e566ed24f749f
pyonotBaloony <206148348+BelgaDevv@users.noreply.github.com>
2026-05-24 22:48:27 -0300

    Preparing workspace

---
+++ src/main/java/com/github/belgadevv/pj_clt_calculator/PjCltCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.github.belgadevv.pj_clt_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjCltCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjCltCalculatorApplication.class, args);
+	}
+
+}
